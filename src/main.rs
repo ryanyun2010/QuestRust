6480fdf807dc22fc2c11495c08eebaa34a6a949c
Ryan <ryanyun2010@gmail.com>
2024-11-29 12:18:07 -0800

    vertex/index buffers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 pub mod window;
 pub mod state;
+pub mod vertex;
 
 fn main() {
     pollster::block_on(window::run());
--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,16 @@
 use winit::window::Window;
 use winit::event::*;
+use wgpu::util::DeviceExt;
+use crate::vertex::Vertex;
+const TEST_INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
+    3, 5, 7,
+    2, 1, 9
+];
+ 
+ 
 
 pub struct State<'a> {
     pub surface: wgpu::Surface<'a>,
@@ -7,6 +18,11 @@ pub struct State<'a> {
     pub queue: wgpu::Queue,
     pub config: wgpu::SurfaceConfiguration,
     pub size: winit::dpi::PhysicalSize<u32>,
+    pub render_pipeline: wgpu::RenderPipeline,
+    pub vertex_buffer: wgpu::Buffer,
+    pub index_buffer: wgpu::Buffer,
+    pub num_indicies: u32,
+    pub TEST_VERTICES: [Vertex; 8],
     window: &'a Window,
 }
 impl<'a> State<'a> { 
@@ -31,7 +47,7 @@ impl<'a> State<'a> {
             &wgpu::DeviceDescriptor {
                 required_features: wgpu::Features::empty(),
                 required_limits: wgpu::Limits::default(),
-                label: None,g
+                label: None,
                 memory_hints: Default::default(),
             },
             None,
@@ -54,6 +70,85 @@ impl<'a> State<'a> {
         };
         surface.configure(&device, &config);
 
+        
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("Shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        });
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+            push_constant_ranges: &[],
+            });
+            let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("Render Pipeline"),
+                layout: Some(&render_pipeline_layout),
+                vertex: wgpu::VertexState {
+                    module: &shader,
+                    entry_point: Some("vs_main"),
+                    buffers: &[
+                        Vertex::desc(),
+                    ],
+                    compilation_options: wgpu::PipelineCompilationOptions::default(),
+                },
+                fragment: Some(wgpu::FragmentState {
+                    module: &shader,
+                    entry_point: Some("fs_main"),
+                    targets: &[Some(wgpu::ColorTargetState {
+                        format: config.format,
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    })],
+                    compilation_options: wgpu::PipelineCompilationOptions::default(),
+                }),
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleList,
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw,
+                    cull_mode: Some(wgpu::Face::Back),
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    unclipped_depth: false,
+                    conservative: false,
+                },
+                depth_stencil: None,
+                multisample: wgpu::MultisampleState {
+                    count: 1,
+                    mask: !0,
+                    alpha_to_coverage_enabled: false,
+                },
+                multiview: None,
+                cache: None,
+            });
+        let mut TEST_VERTICES: [Vertex; 8] = [
+            Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.0] }, // A
+            Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.0] }, // B
+            Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.0] }, // C
+            Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.0] }, // D
+            Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.0] }, // E
+            Vertex { position: [0.7, 0.3, 0.0], color: [0.5, 0.0, 0.0] }, // E
+            Vertex { position: [0.2, 0.6, 0.0], color: [0.5, 0.0, 0.0] }, // E
+            Vertex { position: [0.1, 0.9, 0.0], color: [0.5, 0.0, 0.0] }, // E
+        ];
+        let vertex_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Vertex Buffer"),
+                contents: bytemuck::cast_slice(&mut TEST_VERTICES),
+                usage: wgpu::BufferUsages::VERTEX,
+            }
+        );
+        
+
+        let index_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Index Buffer"),
+                contents: bytemuck::cast_slice(TEST_INDICES),
+                usage: wgpu::BufferUsages::INDEX,
+            }
+        );
+        let num_indicies = TEST_INDICES.len() as u32;
+
         Self {
             window,
             surface,
@@ -61,6 +156,11 @@ impl<'a> State<'a> {
             queue,
             config,
             size,
+            render_pipeline,
+            vertex_buffer,
+            index_buffer,
+            num_indicies,
+            TEST_VERTICES
         }
  
     }
@@ -86,22 +186,35 @@ impl<'a> State<'a> {
     }
 
     pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        self.TEST_VERTICES[0].position[0] += 0.0005;
+        self.TEST_VERTICES[1].position[0] += 0.0004;
+        self.TEST_VERTICES[2].position[0] += 0.0003;
+        self.TEST_VERTICES[3].position[0] += 0.0002;
+        self.vertex_buffer = self.device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Vertex Buffer"),
+                contents: bytemuck::cast_slice(&mut self.TEST_VERTICES),
+                usage: wgpu::BufferUsages::VERTEX,
+            }
+        );
+        
+       
         let output = self.surface.get_current_texture()?;
         let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
         let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
             label: Some("Render Encoder"),
         });
         {
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                     view: &view,
                     resolve_target: None,
                     ops: wgpu::Operations {
                         load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.5,
-                            g: 0.5,
-                            b: 0.0,
+                            r: 1.0,
+                            g: 1.0,
+                            b: 1.0,
                             a: 1.0,
                         }),
                         store: wgpu::StoreOp::Store,
@@ -111,6 +224,10 @@ impl<'a> State<'a> {
                 occlusion_query_set: None,
                 timestamp_writes: None,
             });
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.num_indicies, 0,0..1);
         }
     
         self.queue.submit(std::iter::once(encoder.finish()));
