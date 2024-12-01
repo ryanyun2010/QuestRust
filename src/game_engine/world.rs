45308ec4cb157fe55defe045244687e44d1861e0
Ryan <ryanyun2010@gmail.com>
2024-11-30 17:09:47 -0800

    chunks/world system

--- src/game_engine/world.rs
+++ src/game_engine/world.rs
@@ -1,3 +1,26 @@
+enum Element{
+    Terrain,
+    Entity,
+}
+
+struct Chunk{
+    x: i32,
+    y: i32,
+    elements: mut array: [[Element; 16]; 16]
+}
+
+
+struct World{
+    player: Player,
+    chunks: mut vec<Chunk>
+}
+
+impl World{
+    pub fn getTerrain(&self, x: i32, y: i32) -> Terrain{
+
+    }
+}
+
 struct Terrain{
     x: i32,
     y: i32,
