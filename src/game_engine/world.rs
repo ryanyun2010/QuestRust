f2d6523f6b2acac8824e21ac935629c786247bad
Ryan <ryanyun2010@gmail.com>
2024-11-30 16:49:45 -0800

    add game_engine

---
+++ src/game_engine/world.rs
@@ -0,0 +1,15 @@
+struct Terrain{
+    x: i32,
+    y: i32,
+    id: i32
+}
+
+struct Entity{
+    x: i32,
+    y: i32,
+    id: i32,
+}
+
+struct Player {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use rendering_engine::window;
 use rendering_engine::state;
 use rendering_engine::vertex;
 use rendering_engine::texture;
+pub mod game_engine;
+use game_engine::world;
 
 fn main() {
     pollster::block_on(window::run());
