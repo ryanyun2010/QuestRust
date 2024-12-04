47d094c7636f0f1a3c3c2bc480c5cff7d02d9057
Noah Lev <velnbuild@gmail.com>
2024-12-03 16:34:16 -0800

    Items.

--- src/game_engine/item.rs
+++ src/game_engine/item.rs
@@ -1,10 +1,57 @@
 use crate::game_engine::entities::AttackType;
-#[derive(Clone, Debug)]
-pub struct Item {
-    name: String,
-    lore: String,
-    is_weapon: bool,
-    weapon_type: AttackType,
-    damage: usize, 
-}
+use super::world::Terrain;
 
+#[derive(Clone, Debug)]
+pub enum Item {
+    Lore(String),
+    Weapon(Weapon),
+    Stacking(usize),
+    Place(PlaceTerrain),
+    Use(UseItem),
+    MaxDurability(usize),
+    UseRange(f64),
+}
+#[derive(Clone, Debug)]
+pub enum UseItem {
+    Mining(MiningTool),
+}
+#[derive(Clone, Debug)]
+pub struct MiningTool {
+    can_mine: MiningToolType,
+    mining_speed: f64,
+}
+#[derive(Clone, Debug)]
+pub enum MiningToolType {
+    Pickaxe,
+    Axe,
+    Shovel,
+    Paxel
+}
+#[derive(Clone, Debug)]
+pub enum PlaceTerrain {
+    Terrain(Terrain),
+    Bucket(Terrain),
+    Special(PlaceSpecial)
+}
+#[derive(Clone, Debug)]
+pub enum PlaceSpecial {
+    Bed,
+}
+#[derive(Clone, Debug)]
+pub enum Weapon {
+    Melee(MeleeWeapon),
+    Ranged(RangedWeapon),
+    Magic(MagicWeapon)
+}
+#[derive(Clone, Debug)]
+pub struct MeleeWeapon {
+
+}
+#[derive(Clone, Debug)]
+pub struct RangedWeapon {
+    
+}
+#[derive(Clone, Debug)]
+pub struct MagicWeapon {
+    
+}
