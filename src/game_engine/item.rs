80ffbd8590fc932c34d807075d08ccd8213c3eee
Noah Lev <velnbuild@gmail.com>
2024-12-08 10:21:50 -0800

    more comments

--- src/game_engine/item.rs
+++ src/game_engine/item.rs
@@ -1,36 +1,44 @@
+/*
+BASIC INFORMATION:
+Non-unique items such as materials will rely on tags except for current stack size, which is a component..
+Unique items such as weapons, armor, baubles, etc. will have tags to define their basic behaviour but will also have components for unique attributes.
+Items give stats to the player. Entities and Terrain can have items (specifically the Item Entity type) but they will not be able to use them.
+*/
 use std::os::macos::raw::stat;
 use crate::game_engine::entities::AttackType;
 use crate::stat::GearStat;
 use super::{loot::Rarity, world::{Sprite, Terrain}};
+//Each item when stored will be exactly like this. The item_type_id will be used in a lookup to find the tags.
+//They also have a Vec of components that will be largely unique. It's stackability will not be judged by the components, but instead by the tags.
 #[derive(Clone, Debug)]
 pub struct Item {
     item_type_id: usize,
     component_list: Vec<ItemComponent>,
 }
-#[derive(Clone, Debug)]
-pub enum ItemComponent {
-    Stack(usize),
-    Weapon(WeaponComponent)
-}
+//---THIS IS WHERE TAGS START---
 #[derive(Clone, Debug)]
 pub enum ItemTags {
-    Sprite(Sprite),
-    Lore(String),
-    Weapon(WeaponTag),
-    Place(PlaceTerrain),
-    Use(UseItem),
-    MaxDurability(usize),
-    UseRange(f64),
+    Sprite(Sprite), //No Shit.
+    Lore(String), //Description.
+    Weapon(WeaponTag), //Weapon Stuff.
+    // Place(PlaceTerrain), //I don't think we need this.
+    Use(UseItem), //For special uses like food and maybe things like permenant consumables. This is largely for edge cases.
+    // MaxDurability(usize), //We shouldn't have Durability. Nobody fucking likes it.
+    UseRange(f32), //The range in which this item can be used. Mostly cosmetic, but for uprgrading things in your base, it can be useful.
 }
 #[derive(Clone, Debug)]
 pub enum UseItem {
     Eat(Food)
 }
+//Food is basically our equivalent of potions.
+//Nobody likes brewing.
 #[derive(Clone, Debug)]
 pub struct Food {
     eating_time: f64,
     effects: Option<Vec<StatusEffect>>
 }
+//Might need Ryan's help for implementing the StatusEffect updating system. Only players can have these because yes.
+//If entities can have this, let's make a simpler version made specifically for entities. Entities can't really use items and have different stat systems so...
 #[derive(Clone, Debug)]
 pub struct StatusEffect {
     duration: Option<f64>,
@@ -55,6 +63,8 @@ impl StatusEffect {
         };
     }
 }
+//What type is it?
+//Might not have effect levels.
 #[derive(Clone, Debug)]
 pub enum StatusEffectType {
     Slowness,
@@ -63,37 +73,41 @@ pub enum StatusEffectType {
     Plague
     //We'll do this much later.
 }
-#[derive(Clone, Debug)]
-pub enum PlaceTerrain {
-    Terrain(Terrain),
-    Bucket(Terrain),
-    Special(PlaceSpecial)
-}
-#[derive(Clone, Debug)]
-pub enum PlaceSpecial {
-    Bed,
-}
+// // We probably won't place stuff.
+// #[derive(Clone, Debug)]
+// pub enum PlaceTerrain {
+//     Terrain(Terrain),
+//     Bucket(Terrain),
+//     Special(PlaceSpecial)
+// }
+// // Same with this
+// #[derive(Clone, Debug)]
+// pub enum PlaceSpecial {
+//     Bed,
+// }
+//Weapon info: Weapons are broken up into 3 parts.
+//Will make armor and baubles later.
 #[derive(Clone, Debug)]
 pub enum WeaponTag {
     Melee(MeleeWeaponTag),
     Ranged(RangedWeaponTag),
     Magic(MagicWeaponTag)
 }
+/*
+HOW LEVEL SCALING WORKS:
+quality is out of 100.
+Rarity is out of the Rarity Enumeration:
+Common is 1 to 50, Weight: 50
+Rare is 51 to 80, Weight: 30
+Epic is 81 to 90, Weight: 10
+Mythical is 91 to 97, Weight: 7
+Legendary is 98 to 99, Weight: 2
+SUPREME is 100, Weight: 1
+To upgrade a weapon, one must combine two of equal quality. You will then receive an item of one greater quality, with a random quality in that range. To garuntee a SUPREME item, one must have 32 Commons. Weapon is prioritized over quality.
+base_stat is the stat at quality 1, and base_stat+stat_variation is the maximum stat.
+*/
 #[derive(Clone, Debug)]
 pub struct MeleeWeaponTag {
-    /*
-    HOW LEVEL SCALING WORKS:
-    quality is out of 100.
-    Rarity is out of the Rarity Enumeration:
-    Common is 1 to 50, Weight: 50
-    Rare is 51 to 80, Weight: 30
-    Epic is 81 to 90, Weight: 10
-    Mythical is 91 to 97, Weight: 7
-    Legendary is 98 to 99, Weight: 2
-    SUPREME is 100, Weight: 1
-    To upgrade a weapon, one must combine two of equal quality. You will then receive an item of one greater quality, with a random quality in that range. To garuntee a SUPREME item, one must have 32 Commons. Weapon is prioritized over quality.
-    base_stat is the stat at quality 1, and base_stat+stat_variation is the maximum stat.
-    */
     damage: GearStat,
     attack_speed: GearStat,
     swing_range: GearStat,
@@ -111,6 +125,15 @@ pub enum WeaponComponentType {
     Ranged(RangedWeaponComponent),
     Magic(MagicWeaponComponent),
 }
+//---THIS IS WHERE COMPONENTS START---
+//These are the only two component we have yet.
+#[derive(Clone, Debug)]
+pub enum ItemComponent {
+    Stack(usize),
+    Weapon(WeaponComponent)
+}
+//Weapon shit.
+//Will make armor and baubles later.
 #[derive(Clone, Debug)]
 pub struct WeaponComponent {
     weapon_type: WeaponComponentType,
