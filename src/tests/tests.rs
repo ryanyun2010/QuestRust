eca6439961bcc19dc72eb2842f8634736a040d8c
Ryan <ryanyun2010@gmail.com>
2024-12-27 19:39:47 -0800

    more tests

--- src/tests/tests.rs
+++ src/tests/tests.rs
@@ -38,7 +38,9 @@ pub async fn run(camera: camera::Camera){
 
     println!("TESTS:");
 
-    /* TEST WORLD 1 - NOTHING HAPPENS IN BLANK WORLD */
+    /* ╔════════════════════════════════════════════════════════════════════════════╗
+       ║               TEST SET 1 - ENSURE NOTHING HAPPENS IN BLANK WORLD           ║
+       ╚════════════════════════════════════════════════════════════════════════════╝ */
 
     let mut world = world::World::new(Player::new(596.0, 400.0, 10.0, 10, 1.0, 0));
     world.add_sprite(0);
@@ -54,7 +56,9 @@ pub async fn run(camera: camera::Camera){
     assert_colored_print("Player Y stays constant in blank world", headless.world.player.borrow().y == player_starting_position_y);
     assert_colored_print("Player health stays constant in blank world", headless.world.player.borrow().health == headless.world.player.borrow().max_health as f32);    
 
-    /* TEST WORLD 2 - ENTITIES CAN KILL PLAYER */
+    /* ╔════════════════════════════════════════════════════════════════════════════╗
+       ║                 TEST SET 2 - ENSURE ENTITIES CAN KILL PLAYER               ║
+       ╚════════════════════════════════════════════════════════════════════════════╝ */
 
     let mut world2 = world.clone(); 
     let entity = world2.add_entity(900.0, 405.0);
@@ -77,7 +81,9 @@ pub async fn run(camera: camera::Camera){
     assert_colored_print("Player Y stays constant in world with one entity", headless.world.player.borrow().y == player_starting_position_y);
     assert_colored_print("Player dies in world with entity attacking it", headless.world.player.borrow().health < 0.0);
 
-    /* TEST WORLD 3 - ENTITIES ARE BLOCKED BY TERRAIN */
+    /* ╔════════════════════════════════════════════════════════════════════════════╗
+       ║             TEST SET 3 - ENSURE ENTITIES ARE BLOCKED BY TERRAIN            ║
+       ╚════════════════════════════════════════════════════════════════════════════╝ */
 
     let mut world3 = world.clone();
     for y in 0..25{
@@ -107,7 +113,9 @@ pub async fn run(camera: camera::Camera){
     assert_colored_print("Player Y stays constant in world with terrain blocker and entity", headless.world.player.borrow().y == player_starting_position_y);
     assert_colored_print("Player survives in world with terrain blocker and entity attacking", headless.world.player.borrow().health == headless.world.player.borrow().max_health as f32);
 
-    /* TEST WORLD 4 - ENTITY PATHFINDING */
+    /* ╔════════════════════════════════════════════════════════════════════════════╗
+       ║          TEST SET 4 - ENSURE ENTITIES CAN PATHFIND AROUND TERRAIN          ║
+       ╚════════════════════════════════════════════════════════════════════════════╝ */
 
     let mut world4 = world.clone();
     for y in 5..18{
@@ -137,7 +145,9 @@ pub async fn run(camera: camera::Camera){
     assert_colored_print("Player Y stays constant in world with ineffective terrain blocker and entity", headless.world.player.borrow().y == player_starting_position_y);
     assert_colored_print("Player dies in world with ineffective terrain blocker and entity attacking (entity pathfinding works)", headless.world.player.borrow().health <= 0.0);
 
-    /* TEST WORLD 5 - PLAYER MOVEMENT */
+    /* ╔════════════════════════════════════════════════════════════════════════════╗
+       ║             TEST SET 5 - ENSURE PLAYER MOVEMENT WORKS CORRECTLY            ║
+       ╚════════════════════════════════════════════════════════════════════════════╝ */
 
     let mut world5 = world.clone();
 
@@ -182,7 +192,45 @@ pub async fn run(camera: camera::Camera){
     headless.run(20).await;
     assert_colored_print("Holding both W and S does not move player", headless.world.player.borrow().y == player_starting_y && headless.world.player.borrow().x == player_starting_x);
 
-    /* TEST WORLD 6 - PLAYER COLLISION WITH TERRAIN */
+    headless.state.keys_down.insert(String::from("s"), false);
+    headless.world.player.borrow_mut().y = player_starting_y;
+    headless.world.player.borrow_mut().x = player_starting_x;
+    headless.state.keys_down.insert(String::from("w"), true);
+    headless.state.keys_down.insert(String::from("d"), true);
+    headless.run(20).await;
+    assert_colored_print("Holding both W and D moves player up and right", headless.world.player.borrow().y < player_starting_y && headless.world.player.borrow().x > player_starting_x);
+    assert_colored_print("Holding both W and D moves player up and right equal amounts", player_starting_y - headless.world.player.borrow().y == headless.world.player.borrow().x - player_starting_x);
+    
+    headless.state.keys_down.insert(String::from("a"), true);
+    headless.state.keys_down.insert(String::from("d"), false);
+    headless.world.player.borrow_mut().y = player_starting_y;
+    headless.world.player.borrow_mut().x = player_starting_x;
+    headless.run(20).await;
+    assert_colored_print("Holding both W and A moves player up and left", headless.world.player.borrow().y < player_starting_y && headless.world.player.borrow().x < player_starting_x);
+    assert_colored_print("Holding both W and A moves player up and left equal amounts", player_starting_y - headless.world.player.borrow().y == player_starting_x - headless.world.player.borrow().x);
+
+    headless.state.keys_down.insert(String::from("a"), false);
+    headless.state.keys_down.insert(String::from("d"), true);
+    headless.state.keys_down.insert(String::from("w"), false);
+    headless.state.keys_down.insert(String::from("s"), true);
+    headless.world.player.borrow_mut().y = player_starting_y;
+    headless.world.player.borrow_mut().x = player_starting_x;
+    headless.run(20).await;
+    assert_colored_print("Holding both S and D moves player down and right", headless.world.player.borrow().y > player_starting_y && headless.world.player.borrow().x > player_starting_x);
+    assert_colored_print("Holding both S and D moves player down and right equal amounts", headless.world.player.borrow().y - player_starting_y == headless.world.player.borrow().x - player_starting_x);
+
+    headless.state.keys_down.insert(String::from("a"), true);
+    headless.state.keys_down.insert(String::from("d"), false);
+    headless.world.player.borrow_mut().y = player_starting_y;
+    headless.world.player.borrow_mut().x = player_starting_x;
+    headless.run(20).await;
+    assert_colored_print("Holding both S and A moves player down and left", headless.world.player.borrow().y > player_starting_y && headless.world.player.borrow().x < player_starting_x);
+    assert_colored_print("Holding both S and A moves player down and left equal amounts", headless.world.player.borrow().y - player_starting_y == player_starting_x - headless.world.player.borrow().x);
+    
+
+    /* ╔════════════════════════════════════════════════════════════════════════════╗
+       ║               TEST SET 6 - ENSURE PLAYER COLLIDES WITH TERRAIN             ║
+       ╚════════════════════════════════════════════════════════════════════════════╝ */
 
     let mut world6 = world.clone();
     let terrain_blocker = world6.add_terrain(638, 384);
@@ -219,7 +267,9 @@ pub async fn run(camera: camera::Camera){
     headless.run(200).await;
     assert_colored_print("Moving down does not allow player to move into terrain blocker, but player can move down up to the terrain blocker", headless.world.player.borrow().y == player_starting_y + 10.0);
 
-    /* TEST WORLD 7 - PLAYER COLLISION WITH ENTITIES */
+    /* ╔════════════════════════════════════════════════════════════════════════════╗
+       ║              TEST SET 6 - ENSURE PLAYER COLLIDES WITH ENTITIES             ║
+       ╚════════════════════════════════════════════════════════════════════════════╝ */
 
     let mut world7 = world.clone();
     let entity_blocker = world7.add_entity(638.0, 384.0);
@@ -257,6 +307,9 @@ pub async fn run(camera: camera::Camera){
     assert_colored_print("Moving down does not allow player to move into entity blocker, but player can move down up to the entity blocker", headless.world.player.borrow().y == player_starting_y + 10.0);
 
     
+
+    /* Final Result Printing */
+
     if failcount == 0{
         println!("{} {} {}", "All tests passed in".green(), ((time_tracker.elapsed().as_millis() as f32)/1000.0).to_string().green(), "seconds".green());
     }else if failcount == 1{
