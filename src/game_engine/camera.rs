e1005b90484d922807168bc918da67d54c265784
Ryan <ryanyun2010@gmail.com>
2025-01-17 10:03:57 -0800

    projectiles rotate

--- src/game_engine/camera.rs
+++ src/game_engine/camera.rs
@@ -271,10 +271,12 @@ impl Camera{
                 player_effect_draw_data.vertex.extend(draw_data.vertex);
                 player_effect_draw_data.index.extend(draw_data.index);
                 continue;
+            } else{
+                let draw_data = sprite.unwrap().draw_data_rotated(effect.angle + 90.0, effect.x, effect.y, width.unwrap().floor() as usize, height.unwrap().floor() as usize, self.viewpoint_width, self.viewpoint_height, player_effect_draw_data.vertex.len() as u16, -1 * self.camera_x as i32, -1 * self.camera_y as i32);
+                player_effect_draw_data.vertex.extend(draw_data.vertex);
+                player_effect_draw_data.index.extend(draw_data.index);
+                continue;
             }
-            let draw_data = sprite.unwrap().draw_data(effect.x, effect.y, width.unwrap().floor() as usize, height.unwrap().floor() as usize, self.viewpoint_width, self.viewpoint_height, player_effect_draw_data.vertex.len() as u16, -1 * self.camera_x as i32, -1 * self.camera_y as i32);
-            player_effect_draw_data.vertex.extend(draw_data.vertex);
-            player_effect_draw_data.index.extend(draw_data.index);
         }
         player_effect_draw_data.offset(render_data.vertex.len() as u16);
         render_data.vertex.extend(player_effect_draw_data.vertex);
